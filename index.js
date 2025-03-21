560e99072962131592dbe6ded39f31460e82b53d
Stina Persson <stpe1901@student.miun.se>
2025-03-21 15:19:54 +0100

    push för att posta på render

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost', 
+        host: '0.0.0.0', 
         routes: {
             cors: {
                 origin: ['*'], //Cors ok
